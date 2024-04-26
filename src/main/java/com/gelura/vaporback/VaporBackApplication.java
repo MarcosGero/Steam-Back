bf4134e8232d615568099a6c61c840f5c62e71ce
MarcosGero <92637602+MarcosGero@users.noreply.github.com>
2024-04-26 10:16:36 -0300

    Spring initilization

---
+++ src/main/java/com/gelura/vaporback/VaporBackApplication.java
@@ -0,0 +1,13 @@
+package com.gelura.vaporback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaporBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaporBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gelura/vaporback/VaporBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelura.vaporback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaporBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
