e80d2440af295f57461ebf6067700f6d8abac0fb
MarcosGero <92637602+MarcosGero@users.noreply.github.com>
2024-05-02 12:59:29 -0300

    userRepository con mongodb

--- src/main/java/com/steamer/capas/business/service/impl/UserServiceImpl.java
+++ src/main/java/com/steamer/capas/business/service/impl/UserServiceImpl.java
@@ -1,33 +1,52 @@
 package com.steamer.capas.business.service.impl;
 
 import com.steamer.capas.business.service.UserService;
+import com.steamer.capas.common.exception.UserException;
 import com.steamer.capas.domain.document.User;
+import com.steamer.capas.persistence.UserRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     @Override
     public User create(User user) {
-        return null;
+        return userRepository.save(user);
     }
 
     @Override
     public User getById(Long id) {
-        return null;
+        var optionalUser = userRepository.findById(id);
+
+        //Implementar String.format(UserConstant.TASK_NOT_FOUND_MESSAGE_ERROR, id)
+        if (optionalUser.isEmpty()){
+            throw new UserException(HttpStatus.NOT_FOUND,"Error getById");
+        }
+
+
+        return optionalUser.get();
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
-    public void deleteById(Long id) {}
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
     @Override
     public User update(User user) {
-        return null;
+        return userRepository.save(user);
     }
 }
--- src/main/java/com/steamer/capas/persistence/UserRepository.java
+++ src/main/java/com/steamer/capas/persistence/UserRepository.java
@@ -1,4 +1,10 @@
 package com.steamer.capas.persistence;
 
-public interface UserRepository {
+import com.steamer.capas.domain.document.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
 }
